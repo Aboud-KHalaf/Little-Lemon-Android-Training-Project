240adeff14077a886f6ebcb9f38b1fdba5a5181b
Aboud Khalaf <aboud.khalaf.eng@gmail.com>
2026-06-04 16:44:35 +0300

    finsh desh details

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,20 +1,71 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
-    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        //TODO: Insert code here
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+
+        TopAppBar()
+
+        Image(
+            painter = painterResource(id = dish.imageResource),
+            contentDescription = "Dish image",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+
+        Column(
+            modifier = Modifier.padding(start = 10.dp, end = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+
+            Text(
+                text = dish.name,
+                style = MaterialTheme.typography.h3
+            )
+
+            Text(
+                text = dish.description,
+                style = MaterialTheme.typography.body1
+            )
+
+            Counter()
+
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.add_for) + " $${dish.price}",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
 
@@ -34,12 +85,12 @@ fun Counter() {
         ) {
             Text(
                 text = "-",
-                style = MaterialTheme.typography.h2
+                style = MaterialTheme.typography.h4
             )
         }
         Text(
             text = counter.toString(),
-            style = MaterialTheme.typography.h2,
+            style = MaterialTheme.typography.h4,
             modifier = Modifier.padding(16.dp)
         )
         TextButton(
@@ -49,7 +100,7 @@ fun Counter() {
         ) {
             Text(
                 text = "+",
-                style = MaterialTheme.typography.h2
+                style = MaterialTheme.typography.h4
             )
         }
     }
